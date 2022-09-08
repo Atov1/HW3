d5e49d2f04b69a437149443f1b08d00b8214a1f4
Atov <gognoqqq@gmail.com>
2022-09-08 12:44:13 +0300

    HW3

---
+++ task23/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int J = 1; // (число начала отсчета) по условию задачи, счетчик равен 1
+
+Console.Write($"{N} -> ");
+
+if (N > 0) // проверка знака перед числом (+/-)
+{
+while (J < N) //решение если число положительное
+{
+    Console.Write($"{Math.Pow(J, 3)}, ");
+    J++;
+}
+if (J==N)
+{
+    Console.Write($"{Math.Pow(J, 3)}");
+}}
+else //решение, если число отрицательное
+{
+    J = -1;
+    while (J > N)
+{
+    Console.Write($"{Math.Pow(J, 3)}, ");
+    J--;
+}
+if (J==N)
+{
+    Console.Write($"{Math.Pow(J, 3)}");
+}}
